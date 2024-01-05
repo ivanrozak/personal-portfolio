a358aeff6922fd1d5b5e7889f3904e4200e9e4a1
ivanrozak <ivan.rozak@yesdok.com>
2024-01-05 09:18:21 +0700

    setup meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,15 @@ export const metadata: Metadata = {
   title: "Home | Ivan Rozak",
   description:
     "I am a full-stack software engineer who basically just enjoys creating things.",
+  openGraph: {
+    type: "website",
+    url: "https://ivan-rozak.vercel.app",
+    title: "Ivan Rozak",
+    description:
+      "I am a full-stack software engineer who basically just enjoys creating things.",
+    siteName: "IvanRozak",
+    images: [{ url: "https://ivan-rozak.vercel.app/apple-touch-icon.png?" }],
+  },
 };
 
 export default function RootLayout({
@@ -20,6 +29,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          rel="icon"
+          href="/favicon.ico"
+          type="image/x-icon"
+          sizes="48x48"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png?"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png?"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png?"
+        />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      </head>
       <body className={inter.className} suppressHydrationWarning={true}>
         <Provider>
           <NavigationBar />
