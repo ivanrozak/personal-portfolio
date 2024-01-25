9489dd659057aa9665ce1e5415ab69baeee55419
ivanrozak <ivan.rozak@yesdok.com>
2024-01-25 16:18:53 +0700

    🔥adding blog post with contentful

--- component/ArticleList.tsx
+++ component/ArticleList.tsx
@@ -1,57 +1,22 @@
-"use client";
 import { formatDate } from "@/lib/helpers";
-import { Image } from "@nextui-org/react";
-import React, { useState } from "react";
+import { Link } from "@nextui-org/react";
+import React from "react";
 
 const ArticleList = ({ articles }: { articles: any[] | undefined }) => {
 
   return (
     <>
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-4">
         {articles &&
           articles.map((item, idx) => (
-            <ImageRevealCursor key={idx} item={item} />
+            <div key={idx} className="flex items-center gap-8">
+              <div className="text-second">{formatDate(item.updatedAt)}</div>
+              <Link color="foreground" href={"/blog/" + item.slug}>{item.title}</Link>
+            </div>
           ))}
       </div>
     </>
   );
 };
-
-const ImageRevealCursor = ({ item }: { item: any }) => {
-  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
-  const [isHovered, setHovered] = useState(false);
-  const elRef = React.useRef<HTMLDivElement>(null);
-
-  const handleMouseMove = (e: MouseEvent) => {
-    setCursorPosition({ x: e.clientX, y: e.clientY });
-  };
-
-  const handleHover = () => {
-    setHovered(true);
-  };
-
-  const handleLeave = () => {
-    setHovered(false);
-  };
-  return (
-    <div ref={elRef} onMouseMove={(e: any) => handleMouseMove(e)} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
-      {isHovered && (
-        <div className="absolute pointer-events-none w-[100px]" style={{ left: cursorPosition.x, top: cursorPosition.y, transform: `translate(-50%, -20%)` }}>
-          <Image src={item.heroImage} alt="Reveal Image" />
-        </div>
-      )}
-      <div className="flex gap-8 items-center py-3">
-        <div className="text-second">{formatDate(item.updatedAt)}</div>
-        <a
-
-          href={"/blog/" + item.slug}
-        >
-          <div>{item.title}</div>
-        </a>
-      </div>
-
-    </div>
-  )
-}
 
 export default ArticleList;
